340cd303947d072bce9ccef2ad84285f7c19a17c
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-10-01 23:53:52 -0500

    Renderizando Gif - Prueba

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
+const GIFS = ['https://media4.giphy.com/media/HDR31jsQUPqQo/200w.webp?cid=ecf05e47kwqvpiplwzjz9rck8k60de9ck73hhzfmwf9fw8d8&rid=200w.webp',
+              'https://media2.giphy.com/media/KyBX9ektgXWve/giphy.webp?cid=ecf05e47kwqvpiplwzjz9rck8k60de9ck73hhzfmwf9fw8d8&rid=giphy.webp',
+              'https://media4.giphy.com/media/aUhEBE0T8XNHa/200w.webp?cid=ecf05e47kwqvpiplwzjz9rck8k60de9ck73hhzfmwf9fw8d8&rid=200w.webp'];
+
 function App() {
+  const [gifs, setGifs] = useState(GIFS);
+  /*const state = useState([]);
+  const value = state[0];
+  const updateValue = state[1];*/
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <h1>Giphy</h1>
+        {
+          gifs.map(singleGif => <img src={singleGif} alt='Giphy'/>)
+        }
+
+      </section>
     </div>
   );
 }
