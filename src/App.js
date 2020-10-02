1bc70d377163ac237b237547a434e01f42755f11
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-10-02 00:39:49 -0500

    Peticiones a la API, renderizando Gifs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import getGifs from './services/getGifs';
 
-const GIFS = ['https://media4.giphy.com/media/HDR31jsQUPqQo/200w.webp?cid=ecf05e47kwqvpiplwzjz9rck8k60de9ck73hhzfmwf9fw8d8&rid=200w.webp',
-              'https://media2.giphy.com/media/KyBX9ektgXWve/giphy.webp?cid=ecf05e47kwqvpiplwzjz9rck8k60de9ck73hhzfmwf9fw8d8&rid=giphy.webp',
-              'https://media4.giphy.com/media/aUhEBE0T8XNHa/200w.webp?cid=ecf05e47kwqvpiplwzjz9rck8k60de9ck73hhzfmwf9fw8d8&rid=200w.webp'];
 
 function App() {
-  const [gifs, setGifs] = useState(GIFS);
+  const [gifs, setGifs] = useState([]);
   /*const state = useState([]);
   const value = state[0];
   const updateValue = state[1];*/
+
+  useEffect(function () {
+    getGifs({search:'dog'}).then(gifs => setGifs(gifs))
+  }, [])
 
   return (
     <div className="App">
       <section className="App-content">
         <h1>Giphy</h1>
         {
-          gifs.map(singleGif => <img src={singleGif} alt='Giphy'/>)
+          gifs.map(singleGif => <img src={singleGif} alt='Giphy' />)
         }
 
       </section>
---
+++ src/services/getGifs.js
@@ -0,0 +1,14 @@
+const api_key = 'FdbJxKPfIe8M0TjVcM8c6iMK0HN2NZeP';
+
+export default function getGifs({search = 'luna'} = {}) {
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${search}&limit=25&offset=0&rating=g&lang=en`;
+
+    return fetch(apiUrl)
+        .then(res => res.json())
+        .then(response => {
+            const { data } = response
+            const gifs = data.map(image => image.images.downsized_medium.url)
+            // console.log(gifs)
+            return gifs
+        })
+}
