2501f7c9dc8da0ba7f141830c29c262ee648fe86
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-10-06 13:23:59 -0500

    Update .gitignore

---
+++ build/precache-manifest.83c6ed87f45df5f70e9309cd1b22d5e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91656cd45a540d5696cf2559b8c5da40",
+    "url": "/giffy-app/index.html"
+  },
+  {
+    "revision": "a4b0c8d4c602e827441a",
+    "url": "/giffy-app/static/css/main.1c7a7827.chunk.css"
+  },
+  {
+    "revision": "fa514a64dfbe8db78f32",
+    "url": "/giffy-app/static/js/2.57ebdc1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giffy-app/static/js/2.57ebdc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b0c8d4c602e827441a",
+    "url": "/giffy-app/static/js/main.20df21db.chunk.js"
+  },
+  {
+    "revision": "f7f38635c6cd4364bb79",
+    "url": "/giffy-app/static/js/runtime-main.938a1381.js"
+  }
+]);
